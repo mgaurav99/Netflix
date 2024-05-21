af4cafe3a79001b6bb9c7c45375bb03ee5eae695
mgauravdhasmana99 <mgauravdhasmana99@gmail.com>
2024-05-21 17:20:55 +0530

    Second_commit_fetching api using axios

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,30 +1,91 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import  "./Home.scss";
-import krishna from "../../krishna.jpg"
+//import krishna from "../../krishna.jpg"
+import axios from 'axios';
+import { useState } from 'react';
+
+
+
+const apikey ="ebf3ee31757c650c8fbf1d546cd610c7";
+const url ="https://api.themoviedb.org/3";
+const imgurl ="https://image.tmdb.org/t/p/w500";
+const upcoming ="/discover/movie";
+const genre="genre/movie/list";
 
 
 const Card = ({img}) =>(
+
   
-    <img src={img} alt ="cover" />
+  
+    <img className='card' src={img} alt ="cover" />
   )
 
 
-const Row = ({ title })=>(
+const Row = ({ title, arr =[
+
+
+
+], })=>(
   
-    <div >
+    <div className='row'>
         <h2 >{title}</h2>
 
-        <Card img ={krishna} />
+       <div>
+      {
+        arr.map((item,index)=>(
+          
+           <Card key= {index} img ={`${imgurl}/${item.poster_path}`} /> 
+        ))
+      }
+        
+       </div>
     </div>
   )
 
 const Home = () => {
+
+const [upcomingMovies, setUpcomingMovies]= useState([])
+
+const [genreMovies,setgenreMovies] = useState([])
+
+
+
+ useEffect(()=>{      //call everytime when Home component mount/load, when value of variable in argument changes.
+
+   const fetchUpcoming =async()=> {
+    const { data: {results}} = await axios.get(`${url}/${upcoming}?api_key=${apikey}`); //destructuring
+    
+    setUpcomingMovies(results)
+    
+    console.log(upcomingMovies)
+    
+   };
+
+   const getAllGenre =async()=> {
+    const { data: {results}} = await axios.get(`${url}/${genre}?api_key=${apikey}`); //destructuring
+    
+    setgenreMovies(results)
+    
+    console.log(genreMovies)
+    
+   };
+
+   fetchUpcoming()
+
+ }, [])
+
+
   return (
     <section className='home'>
       
         <div className="banner"></div>
 
-        <Row title={"Popular on Netflix"} />
+        <Row title={"Upcoming Movies"} arr={upcomingMovies} />
+        <Row title={"Popular on Netflix"} arr={upcomingMovies} />
+        <Row title={"Movie"} arr={upcomingMovies} />
+        <Row title={"TV Shows"} arr={upcomingMovies} />
+        <Row title={"Recently Viewed"}  arr={upcomingMovies}/>
+        <Row title={"My List"} arr={upcomingMovies}/>
     </section>
   )
 }
